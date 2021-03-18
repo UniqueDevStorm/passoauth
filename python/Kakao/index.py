6ac0f928a50dbb7c3a02cf96e568620bb1630bf9
UniqueDevStorm <storm@uniquecode.team>
2021-03-18 17:13:12 +0900

    feat: python/Kakao/OAuth2

--- python/Kakao/index.py
+++ python/Kakao/index.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import redirect
+from flask import request
+from flask import jsonify
 
 from dotenv import load_dotenv
 import os
+import requests
 
 app = Flask(__name__)
 
@@ -15,12 +18,16 @@ REDIRECT_URI = 'http://localhost:3000/callback'
 
 @app.route('/')
 def redirected():
-    return redirect(location=f'https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code')
+    return redirect(location=f'https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code&scope=profile account_email')
 
 
 @app.route('/callback')
 def callback():
-    return 'ㅋㅋㄹ'
+    code = request.args.get('code')
+    r = requests.post(f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&code={code}', headers={'Content-Type': 'application/json;charset=UTF-8'})
+    access_token = r.json()['access_token']
+    reque = requests.post('https://kapi.kakao.com/v2/user/me', headers={'Authorization' : f'Bearer {access_token}'})
+    return jsonify(reque.json())
 
 
 app.run(debug=True, host='127.0.0.1', port=3000)
