661e47d7a59df1d3c6d81d8dcb30e0d0546dae9d
UniqueDevStorm <storm@uniquecode.team>
2021-03-18 19:12:14 +0900

    feat: Python Discord OAuth2

--- python/Discord/index.py
+++ python/Discord/index.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import redirect
 from flask import request
+from flask import jsonify
 
 from dotenv import load_dotenv
 import requests
@@ -8,6 +9,8 @@ import os
 
 load_dotenv(verbose=True)
 CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URI = 'http://localhost:3000/callback'
 
 app = Flask(__name__)
 
@@ -21,6 +24,19 @@ def redirected():
 @app.route('/callback')
 def callback():
     code = request.args.get('code')
+    r = requests.post('https://discord.com/api/v8/oauth2/token', data={
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': REDIRECT_URI,
+        'scope': 'identify email'
+    }, headers={'Content-Type': 'application/x-www-form-urlencoded'})
+    access_token = r.json()['access_token']
+    requested = requests.get('https://discord.com/api/v8/users/@me', headers={
+        'Authorization': f'Bearer {access_token}'
+    })
+    return jsonify(requested.json())
 
 
 app.run(host='127.0.0.1', port=3000)
